809e68d96fcf966ec34a997a86edf9b1d846d8c7
Kathryn Engle <kathrynengle@Kathryns-MacBook-Pro.local>
2016-11-02 14:25:34 -0700

    updated clients

--- data/clients.js
+++ data/clients.js
@@ -1,19 +1,20 @@
 class Client {
-    constructor(name, desc) {
+    constructor(name, desc, src) {
         this.name = name;
         //this.date = date;
         this.desc = desc;
+        this.src = src;
     } 
 }
 
 let clients = []
-clients.push(new Client('Youtopia', 'Super fun time'));
-clients.push(new Client('Purple 33', 'Best underground in LA'));
-clients.push(new Client('Youtopia', 'Super fun time'));
-clients.push(new Client('Purple 33', 'Best underground in LA'));
-clients.push(new Client('Youtopia', 'Super fun time'));
-clients.push(new Client('Purple 33', 'Best underground in LA'));
-clients.push(new Client('Youtopia', 'Super fun time'));
-clients.push(new Client('Purple 33', 'Best underground in LA'));
+clients.push(new Client('Youtopia', 'Super fun time', 'zoo.jpg'));
+clients.push(new Client('Purple 33', 'Best underground in LA', 'index.jpg'));
+clients.push(new Client('Youtopia', 'Super fun time', 'zoo.jpg'));
+clients.push(new Client('Purple 33', 'Best underground in LA', 'index.jpg'));
+clients.push(new Client('Youtopia', 'Super fun time', 'zoo.jpg'));
+clients.push(new Client('Purple 33', 'Best underground in LA', 'index.jpg'));
+clients.push(new Client('Youtopia', 'Super fun time', 'zoo.jpg'));
+clients.push(new Client('Purple 33', 'Best underground in LA', 'index.jpg'));
 
 module.exports = clients;
