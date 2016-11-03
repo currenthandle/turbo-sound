faf96ed87b3d2b77a369a2d4930d1f32da9a6728
Ubuntu <ubuntu@ip-172-31-13-244.us-west-1.compute.internal>
2016-11-03 15:27:38 +0000

    updated client styles

--- routes.js
+++ routes.js
@@ -2,9 +2,6 @@ module.exports = (app) => {
     app.get('/', (req, res) => {
         res.render('index');
     });
-    app.get('/about', (req, res) => {
-        res.render('about');
-    });
     app.get('/system', (req, res) => {
         res.render('system');
     });
